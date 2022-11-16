70ff3d70691a1fa075cd59dd6a0574f4a1cc4bd1
Renukadevi ARumugam <renukadevi.arumugam@gmail.com>
2022-11-16 19:40:38 +0000

    saving all files

---
+++ run_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+docker build --tag=finalproject4 .
+
+# Step 2: 
+# List docker images
+docker image ls
+
+# Step 3: 
+# Run flask app
+docker run -p 8000:80 finalproject4
